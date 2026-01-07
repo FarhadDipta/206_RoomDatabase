ac607b43c55a1e905cdc5d31cf64854ec32a3d0c
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-01-07 22:22:26 +0700

    memperbaiki ContainerApp.kt

--- app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
@@ -2,13 +2,14 @@ package com.example.myroom.repositori
 
 import android.app.Application
 import android.content.Context
+import com.example.myroom.room.DatabaseSiswa
 
 interface ContainerApp{
     val RepositoriSiswa : RepositoriSiswa
 }
 
 class ContainerDataApp(private val context: Context):
-    com.example.myroomsatu.repositori.ContainerApp {
+    com.example.myroom.repositori.ContainerApp {
     override val RepositoriSiswa: RepositoriSiswa by lazy {
         OfflineRepositoriSiswa(
             DatabaseSiswa.getDatabase(context).siswaDao()
@@ -17,7 +18,7 @@ class ContainerDataApp(private val context: Context):
 }
 
 class AplikasiSiswa : Application() {
-    lateinit var container: com.example.myroomsatu.repositori.ContainerApp
+    lateinit var container: com.example.myroom.repositori.ContainerApp
 
     override fun onCreate(){
         super.onCreate()
