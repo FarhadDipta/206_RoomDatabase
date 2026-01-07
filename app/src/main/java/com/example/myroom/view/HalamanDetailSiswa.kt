df2f346a5b5845771aa728a5feb655fc109a2cf5
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-01-07 23:18:37 +0700

    menambahkan fungsi BodyDetailDataSiswa

--- app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
@@ -70,3 +70,10 @@ fun DetailSiswaScreen(
         )
     }
 }
+
+@Composable
+private fun BodyDetailDataSiswa(
+    detailSiswaUiState: DetailSiswaUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+)
