9cf75d613769b86b613de5d5b3fddc44dfb49af0
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-01-07 23:43:36 +0700

    menambahkan class DetailSiswa

--- app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
@@ -31,3 +31,10 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false,
 )
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = ""
+)
